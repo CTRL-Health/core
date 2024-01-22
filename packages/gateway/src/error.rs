4ccb2df01bac0685c15285583b7eb698e8d4bdf5
Akanimoh Osutuk <fibrinlab@gmail.com>
2024-01-22 20:44:36 +0000

    feat: added logs

--- packages/gateway/src/error.rs
+++ packages/gateway/src/error.rs
@@ -1,9 +1,11 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
@@ -20,6 +22,46 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // Auth
+            Self::AuthFailedNoAuthTokenCookie
+            | Self::AuthFailedTokenWrongFormat
+            | Self::AuthFailedCtxNotInRequestExt => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+
+
+            // Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            )
+        }
+    }
+}
+
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- packages/gateway/src/main.rs
+++ packages/gateway/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 pub use self::error::{Error, Result};
 use crate::model::ModelController;
+use crate::ctx::Ctx;
+use crate::log::log_request;
 
 use axum::{
     Router, 
@@ -9,18 +11,23 @@ use axum::{
     extract::Query,
     extract::Path,
     middleware,
+    Json,
 };
 use std::net::SocketAddr;
 use serde::Deserialize;
+use serde_json::json;
 use tower_http::services::ServeDir;
 use tower_cookies::{Cookie, CookieManagerLayer, Cookies};
 use tokio::net::TcpListener;
+use uuid::Uuid;
+use axum::http::{Method, Uri};
 
 
 mod ctx;
 mod error;
 mod web;
 mod model;
+mod log;
 
 
 #[tokio::main]
@@ -50,11 +57,43 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn main_response_mapper(res: Response) -> Response {
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
 
+    let uuid = Uuid::new_v4();
+
+    // Get response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            (*status_code, Json(client_error_body)).into_response()
+        }
+    );
+
+    // Server log line
+    let client_error = client_status_error.unzip().1;
+    log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
